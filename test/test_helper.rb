747952fb816cea8a7739a50390c64575640c6d4a
Severin Kaelin <mail@severinkaelin.com>
2018-10-07 17:04:49 +0200

    Resolve deprecation warnings for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,6 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   def sign_in_as(user, password)
-  	post login_path, session: {email: user.email, password: password}
+  	post login_path, params: { session: {email: user.email, password: password} }
   end
 end
